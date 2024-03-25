a9a3648ae1b98be4e0039b7a4ddbc03d723e77fe
MatveiKylosov <matveikyl@mail.ru>
2024-03-25 12:50:22 +0500

    Разработанная структура проекта. Kylosov

--- Startup.cs
+++ Startup.cs
@@ -16,12 +16,17 @@ namespace Shop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+/*            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -34,7 +39,7 @@ namespace Shop
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
-            });
+            });*/
         }
     }
 }
